f10bc4043e4978a628abc8f827be1d15bbeb4556
banseok1216 <bansuk1216@naver.com>
2024-04-06 18:22:49 +0900

    feat: spring cloud gateway, spring cloud eureka, spring cloud -jwt filter-

---
+++ hobbyhobby-eureka/src/main/java/org/example/hobbyhobbyeureka/HobbyhobbyEurekaApplication.java
@@ -0,0 +1,15 @@
+package org.example.hobbyhobbyeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class HobbyhobbyEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HobbyhobbyEurekaApplication.class, args);
+	}
+
+}
