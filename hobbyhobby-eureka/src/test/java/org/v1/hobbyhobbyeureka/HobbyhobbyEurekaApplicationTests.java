c3cc1e4d3a19a940b853308bbd8a6a0169233099
banseok1216 <bansuk1216@naver.com>
2024-04-06 21:39:08 +0900

    fixed erase gradle file

---
+++ hobbyhobby-eureka/src/test/java/org/v1/hobbyhobbyeureka/HobbyhobbyEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.v1.hobbyhobbyeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbyhobbyEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
