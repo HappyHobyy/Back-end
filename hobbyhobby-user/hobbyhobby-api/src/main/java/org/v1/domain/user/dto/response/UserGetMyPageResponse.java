bbb8d607076a1ff986c043928cc0b4db8b287213
banseok1216 <bansuk1216@naver.com>
2024-05-02 15:31:11 +0900

    feat: swagger 내용 추가, getUserMyPageResponse 내용 추가

--- hobbyhobby-user/hobbyhobby-api/src/main/java/org/v1/domain/user/dto/response/UserGetMyPageResponse.java
+++ hobbyhobby-user/hobbyhobby-api/src/main/java/org/v1/domain/user/dto/response/UserGetMyPageResponse.java
@@ -1,18 +1,28 @@
 package org.v1.domain.user.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import org.v1.domain.user.domain.User;
 
 import java.util.List;
 
 public record UserGetMyPageResponse(
+        @Schema(description = "이메일", example = "testEmail")
+        String email,
+        @Schema(description = "닉네임", example = "testNickName")
         String nickname,
-        String email
+        @Schema(description = "성별", example = "MAN/WOMAN")
+        User.UserGender gender,
+        @Schema(description = "내국인/외국인", example = "DOMESTIC/FOREIGNER")
+        User.Nationality nationality
 ) {
     public static UserGetMyPageResponse of(final User user) {
         return new UserGetMyPageResponse(
+                user.getEmail(),
                 user.getNickname(),
-                user.getEmail()
+                user.getUserGender(),
+                user.getNationality()
         );
     }
 }
