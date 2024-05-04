9629701f687bbd631832e105b935620fcdc56aa5
banseok1216 <bansuk1216@naver.com>
2024-05-04 21:43:38 +0900

    fixed: spring security import 오류 해결

--- hobbyhobby-user/hobbyhobby-api/src/main/java/org/v1/global/error/GlobalExceptionHandler.java
+++ hobbyhobby-user/hobbyhobby-api/src/main/java/org/v1/global/error/GlobalExceptionHandler.java
@@ -5,7 +5,6 @@ import org.v1.error.ErrorCode;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingRequestCookieException;
