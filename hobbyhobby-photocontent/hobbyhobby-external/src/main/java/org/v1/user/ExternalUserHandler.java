42bbaf74415a7d3e8d6991e18ea42075a51b12a8
banseok1216 <bansuk1216@naver.com>
2024-05-09 02:40:15 +0900

    fixed: userHandler class 문제 해결

--- hobbyhobby-photocontent/hobbyhobby-external/src/main/java/org/v1/user/ExternalUserHandler.java
+++ hobbyhobby-photocontent/hobbyhobby-external/src/main/java/org/v1/user/ExternalUserHandler.java
@@ -7,8 +7,12 @@ import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 import org.v1.service.UserService;
+
+import java.io.IOException;
 
 @Component
 @AllArgsConstructor
@@ -16,51 +20,30 @@ import org.v1.service.UserService;
 public class ExternalUserHandler {
     private final ObjectMapper objectMapper;
     private final UserService userService;
-    @RabbitListener(
-            bindings = @QueueBinding(
-                    value = @Queue(value = "photocontent"),
-                    exchange = @Exchange(value = "amq.direct", type = "direct"),
-                    key = "photoContent.userCreate"
-            )
-    )
-    public void receiveUserCreate(byte[] payload) {
+    @RabbitListener(queues = "photocontent")
+    public void receiveMessage(byte[] payload, @Header(AmqpHeaders.RECEIVED_ROUTING_KEY) String routingKey) {
         try {
-            UserMessage.CreateUserMessage message = objectMapper.readValue(payload, UserMessage.CreateUserMessage.class);
-            log.info("Received message: {}", message);
-            userService.appendUser(message.toUser());
-        } catch (Exception e) {
-            log.error("Error processing received message.", e);
-        }
-    }
-    @RabbitListener(
-            bindings = @QueueBinding(
-                    value = @Queue(value = "photocontent"),
-                    exchange = @Exchange(value = "amq.direct", type = "direct"),
-                    key = "photoContent.userDelete"
-            )
-    )
-    public void receiveUserDelete(byte[] payload) {
-        try {
-            UserMessage.DeleteUserMessage message = objectMapper.readValue(payload, UserMessage.DeleteUserMessage.class);
-            log.info("Received message: {}", message);
-            userService.removeUser(message.userId());
-        } catch (Exception e) {
-            log.error("Error processing received message.", e);
-        }
-    }
-    @RabbitListener(
-            bindings = @QueueBinding(
-                    value = @Queue(value = "photocontent"),
-                    exchange = @Exchange(value = "amq.direct", type = "direct"),
-                    key = "photoContent.userUpdate"
-            )
-    )
-    public void receiveUserUpdate(byte[] payload) {
-        try {
-            UserMessage.UpdateUserMessage message = objectMapper.readValue(payload, UserMessage.UpdateUserMessage.class);
-            log.info("Received message: {}", message);
-            userService.updateUser(message.toUser());
-        } catch (Exception e) {
+            switch (routingKey) {
+                case "photoContent.userCreate":
+                    UserMessage.CreateUserMessage createUserMessage = objectMapper.readValue(payload, UserMessage.CreateUserMessage.class);
+                    log.info("Received message: {}", createUserMessage);
+                    userService.appendUser(createUserMessage.toUser());
+                    break;
+                case "photoContent.userDelete":
+                    UserMessage.DeleteUserMessage deleteUserMessage = objectMapper.readValue(payload, UserMessage.DeleteUserMessage.class);
+                    log.info("Received message: {}", deleteUserMessage);
+                    userService.removeUser(deleteUserMessage.userId());
+                    break;
+                case "photoContent.userUpdate":
+                    UserMessage.UpdateUserMessage updateUserMessage = objectMapper.readValue(payload, UserMessage.UpdateUserMessage.class);
+                    log.info("Received message: {}", updateUserMessage);
+                    userService.updateUser(updateUserMessage.toUser());
+                    break;
+                default:
+                    log.warn("Unhandled routing key: {}", routingKey);
+                    break;
+            }
+        } catch (IOException e) {
             log.error("Error processing received message.", e);
         }
     }
