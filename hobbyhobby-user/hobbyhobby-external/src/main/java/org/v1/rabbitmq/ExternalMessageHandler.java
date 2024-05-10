33e47a014f910e40183806247da3b6fc984d750b
banseok1216 <114564687+banseok1216@users.noreply.github.com>
2024-05-11 02:39:24 +0900

    Update ExternalMessageHandler.java

--- hobbyhobby-user/hobbyhobby-external/src/main/java/org/v1/rabbitmq/ExternalMessageHandler.java
+++ hobbyhobby-user/hobbyhobby-external/src/main/java/org/v1/rabbitmq/ExternalMessageHandler.java
@@ -31,7 +31,7 @@ public class ExternalMessageHandler {
                     .withPayload(payload)
                     .setHeader("headerKey", header)
                     .build();
-            streamBridge.send("userCached-out-0", msg);
+            streamBridge.send("user-out-0", msg);
         } catch (JsonProcessingException e) {
             log.error("Error serializing message payload: {}", e.getMessage());
         }
