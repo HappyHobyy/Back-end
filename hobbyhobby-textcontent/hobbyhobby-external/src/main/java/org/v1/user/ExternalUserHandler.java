b26071d9058cf55c62a35e3bc9c07d17f4e7f130
banseok1216 <bansuk1216@naver.com>
2024-05-11 02:08:33 +0900

    refact: 유저 resource 파일 수정

--- hobbyhobby-textcontent/hobbyhobby-external/src/main/java/org/v1/user/ExternalUserHandler.java
+++ hobbyhobby-textcontent/hobbyhobby-external/src/main/java/org/v1/user/ExternalUserHandler.java
@@ -9,11 +9,16 @@ import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 import org.v1.service.UserService;
 
 import java.io.IOException;
+import java.util.Objects;
+import java.util.function.Consumer;
 
 @Component
 @AllArgsConstructor
@@ -49,5 +54,38 @@ public class ExternalUserHandler {
         } catch (IOException e) {
             log.error("Error processing received message.", e);
         }
+    }
+
+    @Bean
+    public Consumer<Message<byte[]>> user() {
+        return message -> {
+            try {
+                byte[] payload = message.getPayload();
+                MessageHeaders headers = message.getHeaders();
+                String headerValue = (String) headers.get("headerKey");
+                switch (Objects.requireNonNull(headerValue)) {
+                    case "create":
+                        UserMessage.CreateUserMessage createUserMessage = objectMapper.readValue(payload, UserMessage.CreateUserMessage.class);
+                        log.info("Received message: {}", createUserMessage);
+                        userService.appendUser(createUserMessage.toUser());
+                        break;
+                    case "delete":
+                        UserMessage.DeleteUserMessage deleteUserMessage = objectMapper.readValue(payload, UserMessage.DeleteUserMessage.class);
+                        log.info("Received message: {}", deleteUserMessage);
+                        userService.removeUser(deleteUserMessage.userId());
+                        break;
+                    case "update":
+                        UserMessage.UpdateUserMessage updateUserMessage = objectMapper.readValue(payload, UserMessage.UpdateUserMessage.class);
+                        log.info("Received message: {}", updateUserMessage);
+                        userService.updateUser(updateUserMessage.toUser());
+                        break;
+                    default:
+                        log.warn("Unhandled routing key: {}", payload);
+                        break;
+                }
+            } catch (IOException e) {
+                log.error("Error processing received message.", e);
+            }
+        };
     }
 }
