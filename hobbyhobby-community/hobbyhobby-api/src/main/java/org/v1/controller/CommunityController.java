e173d018f1b3eea05c3e919059e9239cc01b9354
banseok1216 <bansuk1216@naver.com>
2024-05-15 22:54:36 +0900

    feat: 인기 게시물 response 타입변경

--- hobbyhobby-community/hobbyhobby-api/src/main/java/org/v1/controller/CommunityController.java
+++ hobbyhobby-community/hobbyhobby-api/src/main/java/org/v1/controller/CommunityController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.v1.dto.CommunityListResponse;
+import org.v1.dto.ContentResponse;
 import org.v1.dto.UserCommunityListResponse;
 import org.v1.model.community.Community;
 import org.v1.model.content.Contents;
@@ -47,9 +48,9 @@ public class CommunityController {
     @GetMapping("/popular/contents")
     @Operation(summary = "인기 모임/인기 H-Log 40개 가져오기")
     @Parameter(name = "Authorization", description = "Access token", required = true, in = ParameterIn.HEADER)
-    public HttpResponse<Contents> getPopularCommunityPhotoContent(
+    public HttpResponse<ContentResponse> getPopularCommunityPhotoContent(
     ) {
         Contents contents = communityService.getPopularContent();
-        return HttpResponse.success(contents);
+        return HttpResponse.success(ContentResponse.ofContents(contents));
     }
 }
--- hobbyhobby-community/hobbyhobby-storage/src/main/java/org/v1/ContentJpaEntityRepository.java
+++ hobbyhobby-community/hobbyhobby-storage/src/main/java/org/v1/ContentJpaEntityRepository.java
@@ -3,18 +3,26 @@ package org.v1;
 import org.springframework.stereotype.Repository;
 import org.v1.model.community.Community;
 import org.v1.model.content.Contents;
+import org.v1.model.content.GroupArticle;
+import org.v1.model.content.PhotoArticle;
 import org.v1.repository.ContentRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class ContentJpaEntityRepository implements ContentRepository {
     @Override
     public Contents.PhotoArticles readPopularPhotoContent(Community community) {
-        return null;
+        List<PhotoArticle> popularPhotoArticle = new ArrayList<>();
+        return new Contents.PhotoArticles(popularPhotoArticle,popularPhotoArticle);
     }
+
 
     @Override
     public Contents.GroupArticles readPopularGroupContent(Community community) {
-        return null;
+        List<GroupArticle> groupArticles = new ArrayList<>();
+        return new Contents.GroupArticles(groupArticles,groupArticles);
     }
 
     @Override
