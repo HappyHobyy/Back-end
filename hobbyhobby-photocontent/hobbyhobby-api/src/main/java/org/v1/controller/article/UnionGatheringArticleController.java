133e4cb04d8d7437f27640fa794772169ab265f4
Nutria <qkrwldn2016@gmail.com>
2024-05-26 20:30:43 +0900

    Revert "change unionGatheringSearch with input param. Add community search with 1 community"
    This reverts commit 3677d75946242115701d38c5eb96b52abf668b40.

--- hobbyhobby-photocontent/hobbyhobby-api/src/main/java/org/v1/controller/article/UnionGatheringArticleController.java
+++ hobbyhobby-photocontent/hobbyhobby-api/src/main/java/org/v1/controller/article/UnionGatheringArticleController.java
@@ -42,27 +42,14 @@ public class UnionGatheringArticleController {
     }
 
     @GetMapping("/search")
-    @Operation(summary = "연합 모임 게시글 검색 : 커뮤니티 1개")
-    @Parameter(name = "Authorization", description = "Access token", required = true, in = ParameterIn.HEADER)
-    public HttpResponse<List<UnionGatheringArticleResponse>> getUnionGatheringSearch(
-            @Schema(description = "게시물 index", example = "0")
-            @RequestHeader Integer index,
-            @Schema(description = "커뮤니티Id1", example = "123")
-            @RequestHeader Integer communityId1
-    ) {
-        List<GatheringArticle> gatheringArticles = service.getTenArticleSearch(index, GatheringInfo.unionGatheringWithCommunity(List.of(communityId1)));
-        return HttpResponse.success(UnionGatheringArticleResponse.of(gatheringArticles));
-    }
-
-    @GetMapping("/search")
-    @Operation(summary = "연합 모임 게시글 검색 : 커뮤니티 2개")
+    @Operation(summary = "연합 모임 게시글 검색 가져오기 max 10개")
     @Parameter(name = "Authorization", description = "Access token", required = true, in = ParameterIn.HEADER)
     public HttpResponse<List<UnionGatheringArticleResponse>> getUnionGatheringSearch(
             @Schema(description = "게시물 index", example = "0")
             @RequestHeader Integer index,
             @Schema(description = "커뮤니티Id1", example = "123")
             @RequestHeader Integer communityId1,
-            @Schema(description = "커뮤니티Id2", example = "456")
+            @Schema(description = "커뮤니티Id2", example = "123")
             @RequestHeader Integer communityId2
     ) {
         List<GatheringArticle> gatheringArticles = service.getTenArticleSearch(index, GatheringInfo.unionGatheringWithCommunity(List.of(communityId1,communityId2)));
--- hobbyhobby-photocontent/hobbyhobby-storage/src/main/java/org/v1/jpaentityrepository/gathering/GatheringArticleJpaEntityRepository.java
+++ hobbyhobby-photocontent/hobbyhobby-storage/src/main/java/org/v1/jpaentityrepository/gathering/GatheringArticleJpaEntityRepository.java
@@ -60,22 +60,12 @@ public class GatheringArticleJpaEntityRepository implements GatheringArticleRepo
         Pageable pageable = PageRequest.of(index, 10);
         return switch (info.type()) {
             case UNION_GATHERING -> {
-                if (info.communityIds().size() == 1)
-                {
-                    Page<UnionGatheringDetailJpaEntity> unionEntities =
-                            unionGatheringDetailJpaRepository.findAllByCommunityIdAndCreatedAtAfterOrderByDesc(info.communityIds().get(0).longValue(), pageable);
-                    yield unionEntities.isEmpty() ? new ArrayList<>() :
-                            unionEntities.stream()
-                                    .map(UnionGatheringDetailJpaEntity::toGatheringArticle)
-                                    .collect(Collectors.toList());
-                } else {
-                    Page<UnionGatheringDetailJpaEntity> unionEntities =
-                            unionGatheringDetailJpaRepository.findAllByCommunityIdAndCreatedAtAfterOrderByDesc(info.communityIds().get(0).longValue(),info.communityIds().get(1).longValue(), pageable);
-                    yield unionEntities.isEmpty() ? new ArrayList<>() :
-                            unionEntities.stream()
-                                    .map(UnionGatheringDetailJpaEntity::toGatheringArticle)
-                                    .collect(Collectors.toList());
-                }
+                Page<UnionGatheringDetailJpaEntity> unionEntities =
+                        unionGatheringDetailJpaRepository.findAllByCommunityIdAndCreatedAtAfterOrderByDesc(info.communityIds().get(0).longValue(),info.communityIds().get(1).longValue(), pageable);
+                yield unionEntities.isEmpty() ? new ArrayList<>() :
+                        unionEntities.stream()
+                                .map(UnionGatheringDetailJpaEntity::toGatheringArticle)
+                                .collect(Collectors.toList());
             }
             case SINGLE_GATHERING -> {
                 Page<GatheringDetailJpaEntity> entities =
