73bf49c60ae229c7aa62e9deff03a340c5103d27
banseok1216 <bansuk1216@naver.com>
2024-05-30 17:45:32 +0900

    fixed: 단일 모임 생성 request dto 수정

--- hobbyhobby-photocontent/hobbyhobby-api/src/main/java/org/v1/dto/request/SingleGatheringArticleRequest.java
+++ hobbyhobby-photocontent/hobbyhobby-api/src/main/java/org/v1/dto/request/SingleGatheringArticleRequest.java
@@ -62,7 +62,7 @@ public record SingleGatheringArticleRequest(
         public GatheringArticle toArticle(Long userId,MultipartFile file) {
             try {
                 File convertedFile = FileUtil.convertMultipartFileToFile(file);
-                ImageVideo image = ImageVideo.withoutPath(0, convertedFile, ImageVideo.FileType.UNION_GATHERING);
+                ImageVideo image = ImageVideo.withoutPath(0, convertedFile, ImageVideo.FileType.GATHERING);
                 return GatheringArticle.initial(User.onlyUserId(userId), this.title, GatheringInfo.unionGatheringWithCommunity(List.of(communityId)),image,joinMax);
             } catch (IOException e) {
                 throw new RuntimeException("MultipartFile -> File로 전환이 실패했습니다.", e);
